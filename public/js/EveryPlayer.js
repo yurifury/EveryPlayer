25c6211ce5bb68fbdb09fc8379b9d6e96d596178
Yuri Feldman <yuri@tbqh.net>
2011-08-24 03:29:41 +1000

    spec/implement the movement of the Master across MediaClips

--- public/js/EveryPlayer.js
+++ public/js/EveryPlayer.js
@@ -1,13 +1,17 @@
 (function($) {
 	
 	window.MediaClip = Backbone.Model.extend({
-		
-		intialize: function() {
-			this.player = this.options.player;
-		},
-		
+
+    initialize: function() {
+      this.player = new Player();
+    },
+    
 		relativeEnd: function() {
 			return this.get('in') + this.get('duration');
+		},
+		
+		shouldBePlaying: function(time) {
+		  return (this.get('in') <= time && this.relativeEnd() > time);
 		}
 		
 	});
@@ -22,29 +26,100 @@
 				return clip.relativeEnd();
 			});
 			return last_clip.relativeEnd();
+		},
+		
+		playClips: function(time) {
+      _(this.shouldBePlaying(time)).each(function(clip) {
+        clip.player.play();
+      });
+
+      _(this.shouldNotBePlaying(time)).each(function(clip) {
+        clip.player.stop();
+      });
+		},
+		
+		shouldBePlaying: function(time) {
+		  return this.select(function(clip) {
+		    return clip.shouldBePlaying(time);
+		  });
+		},
+		
+		shouldNotBePlaying: function(time) {
+		  return this.reject(function(clip) {
+		    return clip.shouldBePlaying(time);
+		  });
+		},
+		
+		stop: function() {
+		  this.each(function(clip) {
+		    clip.player.stop();
+		  });
 		}
 
 	});
 	
-	window.Master = Backbone.Model.extend({
+	window.Player = Backbone.Model.extend({
+
+	  defaults: {
+	    state: 'stop'
+	  },
+	  
+	  play: function() {
+	    this.set({state: 'play'});
+	  },
+	  
+	  stop: function() {
+	    this.set({state: 'stop'});
+	  },
+	  
+	  isPlaying: function() {
+      return (this.get('state') == 'play');
+	  },
+	  
+	  isStopped: function() {
+	    return (!this.isPlaying());
+	  }
+	  
+	});
+	
+	
+	window.Master = Player.extend({
+	  initialize: function() {
+	    this.mediaclips = this.get('mediaclips');
+      this.set({duration: this.mediaclips.masterLength()});
+      this.bind("change:time", this.setTime);
+	  },
 		
-		initialize: function() {
-			this.set({duration: this.get('mediaclips').masterLength()});
+		defaults: {
+	    time: 0
+		},
+		
+		play: function() {
+		  this.set({state: 'play'});
+		  this.mediaclips.playClips(this.get('time'));
+		},
+		
+		stop: function() {
+		  this.set({state: 'stop'});
+		  this.mediaclips.stop();
+		},
+		
+		setTime: function() {
+		  if (this.isPlaying()) {
+        this.play();
+      }
+      else {
+        this.stop();
+      }
 		}
 		
 	});
 	
-	window.Player = Backbone.Model.extend({});
-	
 	$(document).ready(function() {
 		
-		window.MediaClipView = Backbone.View.extend({
-			
-			initialize: function() {
-				this.player = this.options.player;
-			}
-			
-		});
+		window.MediaClipView = Backbone.View.extend({});
+		
+		window.MasterView = Backbone.View.extend({});
 		
 	});
 	
--- test-js/spec/EveryPlayerSpec.js
+++ test-js/spec/EveryPlayerSpec.js
@@ -1,13 +1,16 @@
 var mediaData = [{
+  "id": 1,
 	"title": "Viewpoint A",
 	"in": 0,
 	"duration": 30
 }, {
-	"title": "Viewpoint B",
+  "id": 2,
+  "title": "Viewpoint B",
 	"in": 20,
 	"duration": 20
 }, {
-	"title": "Viewpoint C",
+  "id": 3,
+  "title": "Viewpoint C",
 	"in": 0,
 	"duration": 80
 }];
@@ -24,6 +27,22 @@ describe ("MediaClip", function() {
 	
 	it("should return relative endtime", function() {
 		expect(this.mediaclip.relativeEnd()).toEqual(30);
+	});
+	
+	describe("shouldBePlaying", function() {
+	  
+	  it("should be playing at t=in", function() {
+	    expect(this.mediaclip.shouldBePlaying(0)).toBeTruthy();
+	  });
+	  
+	  it("should be playing at in < t < duration", function() {
+	    expect(this.mediaclip.shouldBePlaying(5)).toBeTruthy();
+	  });
+	  
+	  it("should not be playing at t=duration", function() {
+	    expect(this.mediaclip.shouldBePlaying(30)).toBeFalsy();
+	  });
+	  
 	});
 
 });
@@ -43,7 +62,7 @@ describe("MediaClips", function() {
 		expect(this.mediaclips.masterLength()).toEqual(30);
 	});
 	
-	  	it("calculates master length for two overlaying clips", function() {
+	it("calculates master length for two overlaying clips", function() {
 		this.mediaclips.add(mediaData[1]);
 		expect(this.mediaclips.masterLength()).toEqual(40);
 	});
@@ -57,20 +76,41 @@ describe("MediaClips", function() {
 });
 
 describe("Player", function() {
+  
+  describe("playback predicates", function() {
+    
+    beforeEach(function() {
+      this.player = new Player();
+    });
+    
+    it("correctly reports play state", function() {
+      expect(this.player.isPlaying()).toBeFalsy();
+    });
+    
+    it("correctly reports stop state", function() {
+      expect(this.player.isStopped()).toBeTruthy();
+    });
+    
+    it("gets stopped by stop event", function() {
+      this.player.stop();
+      expect(this.player.isStopped()).toBeTruthy();
+    });
+    
+    it("gets played by play event", function() {
+      this.player.play();
+      expect(this.player.isPlaying()).toBeTruthy();
+    });
+    
+  });
 	
-	describe("belonging to a MediaClipView", function() {
+	describe("belonging to a MediaClip", function() {
 		
 		beforeEach(function() {
-			this.player = new Player();
 			this.mediaclip = new MediaClip(mediaData[0]);
-			this.mediaclipview = new MediaClipView({
-				model: this.mediaclip,
-				player: this.player
-			});
 		});
 		
 		it("should be accessible from MediaClip", function() {
-			expect(this.mediaclipview.player).toBe(this.player);
+			expect(this.mediaclip.player).toBeDefined();
 		});
 		
 	});
@@ -84,8 +124,114 @@ describe("Master Timeline", function() {
 		this.master = new Master({mediaclips: this.mediaclips});
 	});
 	
+	it("starts at t=0", function() {
+    expect(this.master.get('time')).toEqual(0);
+	});
+	
 	it("inherits duration from MediaClips", function() {
 		expect(this.master.get('duration')).toEqual(80);
 	});
-  
+	
+	it("should not be playing by default", function() {
+	  expect(this.master.isStopped()).toBeTruthy();
+	});
+	
+	describe("playback controls", function() {
+	  
+	  it("should play through the play method", function() {
+	    this.master.play();
+	    expect(this.master.isPlaying()).toBeTruthy();
+	  });
+	  
+	  it("should stop through the stop method", function() {
+	    this.master.stop();
+	    expect(this.master.isStopped()).toBeTruthy();
+	  });
+	  
+	});
+	
+	describe("playing", function() {
+	  
+	  beforeEach(function() {
+	    this.master.play();
+	  });
+	  
+	  describe("t=0", function() {
+
+  	  it("A is playing", function() {
+  	    expect(this.master.mediaclips.get(1).player.isPlaying()).toBeTruthy();
+  	  });
+  	  
+  	  it("B is stopped", function() {
+  	    expect(this.master.mediaclips.get(2).player.isPlaying()).toBeFalsy();
+  	  });
+  	  
+  	  it("C is playing", function() {
+        expect(this.master.mediaclips.get(3).player.isPlaying()).toBeTruthy();
+  	  });
+  	  
+	  });
+	  
+	  describe("t=30", function() {
+	    
+	    beforeEach(function() {
+        this.master.set({time: 30});
+	    });
+
+  	  it("A has stopped", function() {
+  	    expect(this.master.mediaclips.get(1).player.isPlaying()).toBeFalsy();
+  	  });
+  	  
+  	  it("B is playing", function() {
+  	    expect(this.master.mediaclips.get(2).player.isPlaying()).toBeTruthy();
+  	  });
+  	  
+  	  it("C is playing", function() {
+        expect(this.master.mediaclips.get(3).player.isPlaying()).toBeTruthy();
+  	  });
+  	  
+	  });
+	  
+	  describe("t=40", function() {
+	    
+	    beforeEach(function() {
+        this.master.set({time: 40});
+	    });
+
+  	  it("A has stopped", function() {
+  	    expect(this.master.mediaclips.get(1).player.isPlaying()).toBeFalsy();
+  	  });
+  	  
+  	  it("B has stopped", function() {
+  	    expect(this.master.mediaclips.get(2).player.isPlaying()).toBeFalsy();
+  	  });
+  	  
+  	  it("C is playing", function() {
+        expect(this.master.mediaclips.get(3).player.isPlaying()).toBeTruthy();
+  	  });
+  	  
+	  });
+	  
+	  describe("t=80", function() {
+	    
+	    beforeEach(function() {
+        this.master.set({time: 80});
+	    });
+
+  	  it("A has stopped", function() {
+  	    expect(this.master.mediaclips.get(1).player.isPlaying()).toBeFalsy();
+  	  });
+  	  
+  	  it("B has stopped", function() {
+  	    expect(this.master.mediaclips.get(2).player.isPlaying()).toBeFalsy();
+  	  });
+  	  
+  	  it("C has stopped", function() {
+        expect(this.master.mediaclips.get(3).player.isPlaying()).toBeFalsy();
+  	  });
+  	  
+	  });
+	  
+	});
+	
 });
